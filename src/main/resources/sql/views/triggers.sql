b6b237465a2466f2c7e982cf81619d97cf59504c
Chris <chris.bitmead@gmail.com>
2023-03-09 16:15:55 +1100

    NSL-4456 add triger to regenerate cached_synonymy_html

--- src/main/resources/sql/views/triggers.sql
+++ src/main/resources/sql/views/triggers.sql
@@ -173,3 +173,27 @@ CREATE TRIGGER instance_insert_delete
     AFTER INSERT OR DELETE ON instance
     FOR EACH ROW
 EXECUTE PROCEDURE instance_notification();
+
+-- create trigger function
+CREATE OR REPLACE FUNCTION update_synonyms_and_cache()
+    RETURNS TRIGGER AS $$
+BEGIN
+    IF TG_OP = 'INSERT' OR NEW.instance_type_id <> OLD.instance_type_id THEN
+        UPDATE instance
+        SET cached_synonymy_html = coalesce(synonyms_as_html(instance.id), '<synonyms></synonyms>')
+        WHERE instance.id=NEW.id;
+        UPDATE instance
+        SET cached_synonymy_h7tml = coalesce(synonyms_as_html(instance.id), '<synonyms></synonyms>')
+        WHERE instance.id IN (SELECT cited_by_id FROM instance WHERE instance.id=NEW.id);
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Drop trigger
+DROP TRIGGER IF EXISTS update_instance_synonyms_and_cache on instance;
+
+-- create trigger
+CREATE TRIGGER update_instance_synonyms_and_cache
+    AFTER INSERT OR UPDATE ON instance
+    FOR EACH ROW EXECUTE PROCEDURE update_synonyms_and_cache();
