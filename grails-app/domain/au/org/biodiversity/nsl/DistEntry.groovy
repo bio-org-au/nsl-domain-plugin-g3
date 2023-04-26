b585f10cdea2aa8f75c15951d4c2f0aa59f9b337
Chris Bitmead <chris.bitmead@gmail.com>
2023-04-26 16:50:53 +0800

    NSL-4494 - fix distribution entries

--- grails-app/domain/au/org/biodiversity/nsl/DistEntry.groovy
+++ grails-app/domain/au/org/biodiversity/nsl/DistEntry.groovy
@@ -59,6 +59,6 @@ class DistEntry implements Comparable<DistEntry> {
 
     @Override
     int compareTo(DistEntry o) {
-        return id ? o.id ? id.compareTo(o.id) : 1 : -1
+        return id ? o?.id ? id.compareTo(o.id) : 1 : -1
     }
 }
