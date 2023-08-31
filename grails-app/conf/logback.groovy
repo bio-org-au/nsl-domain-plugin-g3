1c7b79be288e4e4c9370c09f71c5d5964db39ca6
Chris Bitmead <chris.bitmead@gmail.com>
2023-08-31 17:51:54 +0800

    java 17

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -1,40 +1,12 @@
-import grails.util.BuildSettings
-import grails.util.Environment
-import org.springframework.boot.logging.logback.ColorConverter
-import org.springframework.boot.logging.logback.WhitespaceThrowableProxyConverter
-
-import java.nio.charset.Charset
-
-conversionRule 'clr', ColorConverter
-conversionRule 'wex', WhitespaceThrowableProxyConverter
-
-// See http://logback.qos.ch/manual/groovy.html for details on configuration
 appender('STDOUT', ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
-        charset = Charset.forName('UTF-8')
-
         pattern =
-                '%clr(%d{yyyy-MM-dd HH:mm:ss}){magenta} ' + // Date
-                        '%clr(%5p) ' + // Log level
-                        '%clr([%4.5t]){green} ' + // Thread
-                        '%clr(%-15.20logger{39}){cyan} %clr(:){faint} ' + // Logger
-                        '%msg %ex{1}%nopex%n' // Message
+                '%level %logger %message%n%xException'
     }
-    logger("StackTrace", ERROR, ['STDOUT'], false)
-    logger("org.hibernate.orm.deprecation", OFF)
-    root(WARN, ['STDOUT'])
 }
-
-def targetDir = BuildSettings.TARGET_DIR
-// println ("ENV is ${Environment.getCurrent()}")
-if (Environment.isDevelopmentMode() && targetDir != null) {
-    root(DEBUG, ['STDOUT'])
-    logger("au.org.biodiversity", DEBUG, ['STDOUT'], false)
-    logger("services3", DEBUG, ['STDOUT'], false)
-}
-
-if (Environment.getCurrent() == Environment.PRODUCTION && targetDir != null) {
-    root(ERROR, ['dailyFileAppender'])
-    logger("au.org.biodiversity", DEBUG, ['dailyFileAppender'], false)
-    logger("services3", DEBUG, ['dailyFileAppender'], false)
-}
+logger("StackTrace", ERROR, ['STDOUT'], false)
+logger("au.gov", DEBUG, ['STDOUT'], false)
+logger("au.org.biodiversity", DEBUG, ['STDOUT'], false)
+logger("grails.plugin.externalconfig", INFO, ['STDOUT'], false)
+logger("org.hibernate.orm.deprecation", OFF)
+root(WARN, ['STDOUT'])
